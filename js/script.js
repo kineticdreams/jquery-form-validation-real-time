af640e3c1342e44936dcba111389e7401e7de46b
Liviu Schera <liviu.schera@gmail.com>
2017-05-13 15:15:50 +0930

    revision 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     var validEmail, validPhone, validUrl = false;
     var passLen = false;
     var passNo = false;
+    var placeholder = $('input[placeholder]').text();
 
 //     $(function()
 //     {
@@ -20,7 +21,6 @@ $(document).ready(function () {
 //     });
 
     function checkIfValid(value) {
-        console.log("this = ", this);
         if (!value) {
             $('#error').removeClass('valid').addClass('invalid');
             return false;
@@ -32,21 +32,20 @@ $(document).ready(function () {
     }
 
     $('#email').keyup(function () {
-        var span = $(this).next();
+
         var pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         var email = $(this).val();
         var isValid = pattern.test(email);
-        // console.log('error: ', $('#email + span').val());
-        if (span.val() === ''){
-            span.addClass('on');
-        }
         validEmail = checkIfValid(isValid);
-        // console.log("email", validEmail);
+console.log(validEmail);
     }).focus(function () {
-        $(this).next().html("<strong id='error'>Please enter a valid email address!</strong>");
-        $('#formError').html("<p id='error'>Please enter a valid email address!</p>").show();
+        $(this).next().html("<p id='error'>Please enter a valid email address!</p>").show("slow");
     }).blur(function () {
-        $('#formError').hide();
+        if (validEmail === false) {
+            $(this).focus();
+            $(this).next().html('<p id="error">Entry does not match expected pattern: ' + $(this).attr('placeholder') + '</p>');
+        } // isValid test
+        $(this).next().hide("slow");
     });
 
 
@@ -67,11 +66,11 @@ $(document).ready(function () {
             $('#number').removeClass('valid').addClass('invalid');
 
         }
-        console.log(passLen, passNo);
+
     }).focus(function () {
-        $('#formError').html('<p>Password requirements: at least <span id="number" class="bold invalid">one number</span> and at least <span id="length" class="bold invalid">6 characters</span></p>').show();
+        $(this).next().html('<p id="error">At least <span id="number" class="bold invalid">one number</span> and at least <span id="length" class="bold invalid">6 characters</span></p>').show("slow");
     }).blur(function () {
-        $('#formError').hide();
+        $(this).next().hide("slow");
     });
 
 
@@ -79,45 +78,40 @@ $(document).ready(function () {
         var phone = $(this).val();
         var telPattern = /^04 ?(?:(?:[01] ?[0-9]|2 ?[0-57-9]|3 ?[1-9]|4 ?[7-9]|5 ?[018]) ?[0-9]|3 ?0 ?[0-5])(?: ?[0-9]){5}$/i;
         var isValid = telPattern.test(phone);
-        // console.log(phone, isValid);
         validPhone = checkIfValid(isValid);
-        console.log("valid phone", validPhone);
         return validPhone;
-
     }).focus(function () {
-        $('#formError').html("<p id='error'>Please enter a valid phone number!</p>").show();
+        $(this).next().html("<p id='error'>Please enter a valid phone number!</p>").show("slow");
     }).blur(function () {
-        $('#formError').hide();
+        $(this).next().hide("slow");
     });
 
 
     $('#myurl').keyup(function () {
         var url = $(this).val();
-        var urlPattern = /^http\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/S*)?$/i;
+        var urlPattern = /^http:\/\/[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/S*)?$/i;
         var isValid = urlPattern.test(url);
-        console.log(isValid);
         validUrl = checkIfValid(isValid);
     }).focus(function () {
-        $('#formError').html("<p id='error'>Please enter a valid url address!</p>").show();
+        $(this).next().html("<p id='error'>Please enter a valid url address!</p>").show("slow");
     }).blur(function () {
         var url = $(this).val();
-        console.log(url);
         if (url.indexOf("http://")) {
             url = "http://" + url;
             $("#myurl").val(url);
-            console.log(url);
         }
-        $('#formError').hide();
+        $(this).next().hide("slow");
     });
 
     $('#myform').submit(function () {
         var abort = false;
         $('input[placeholder]').each(function () {
             // console.log(index,":", value, ":", $(this).val());
-            if ($(this).val() === '' || validPhone === false || validEmail === false || validUrl === false || passNo === false || passLen === false) {
-                $(this).next().html('This is a required field!').addClass("on");
-
-                // console.log($(this).after());
+            if ($(this).val() === '') {
+                $(this).next().html('This is a required field!').show("1000");
+                abort = true;
+            }
+            if (validPhone === false || validEmail === false || validUrl === false || passNo === false || passLen === false){
                 abort = true;
             }
         }); // go through each required value
